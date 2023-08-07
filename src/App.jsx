aeccddfc09f3b34131c46b63fe3519f2bde3a60f
SANGWON LEE <yonga3749@naver.com>
2023-08-07 13:18:55 +0900

    Create DarkModeContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList/TodoList';
 import Header from './components/Header/Header';
+import { DarkModeProVider } from './context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProVider>
       <Header filters={filters} filter={filter} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProVider>
   );
 }
 
